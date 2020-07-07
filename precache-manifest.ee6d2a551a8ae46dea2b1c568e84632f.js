d58632e8668ffadaba6bb1d2d8a4769aa9678cdb
kevinkellyyyy <kellyisyanta@gmail.com>
2020-07-08 03:40:07 +0700

    Updates

---
+++ precache-manifest.ee6d2a551a8ae46dea2b1c568e84632f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc1d3648515f35343dbf8761f190aaf",
+    "url": "/todo-coba/index.html"
+  },
+  {
+    "revision": "f1a5e2c5a7367f38e8e5",
+    "url": "/todo-coba/static/css/main.6924f5ca.chunk.css"
+  },
+  {
+    "revision": "b5b1f202b6a9a737039b",
+    "url": "/todo-coba/static/js/2.b5763d74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-coba/static/js/2.b5763d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a5e2c5a7367f38e8e5",
+    "url": "/todo-coba/static/js/main.3e82d054.chunk.js"
+  },
+  {
+    "revision": "9748ab7f75658112ffb6",
+    "url": "/todo-coba/static/js/runtime-main.e5e183f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-coba/static/media/logo.5d5d9eef.svg"
+  }
+]);
